ed2264a26424f47e00db8c9c67f1e36cbbec9e69
Кузин Павел Евгеньевич <KuzinPE@gnivc.msk>
2024-12-05 01:37:26 +0300

    add script

---
+++ merge.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+
+// Получаем список всех Markdown-файлов в текущей директории
+const files = fs
+  .readdirSync(__dirname)
+  .filter((file) => file.endsWith('.md') && file !== 'README.md');
+
+// Сортируем файлы по имени (опционально)
+files.sort();
+
+// Читаем содержимое файлов и объединяем их
+let combinedContent = '';
+files.forEach((file) => {
+  const content = fs.readFileSync(path.join(__dirname, file), 'utf8');
+  combinedContent += `\n\n## ${file}\n\n` + content; // Добавляем заголовок с именем файла
+});
+
+// Записываем результат в README.md
+fs.writeFileSync(path.join(__dirname, 'README.md'), combinedContent, 'utf8');
+
+console.log('Файлы объединены в README.md');
